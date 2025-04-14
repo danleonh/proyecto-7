934eba3d694dd3882dc13275d79dcc0b9a702f24
danleonh <edleojar@hotmail.com>
2025-04-13 21:28:27 -0700

    se agrego el boton del grafico de dispersion

--- app.py
+++ app.py
@@ -9,7 +9,14 @@ car_data = pd.read_csv('/Users/d.leon/Desktop/TRIPLETEN/proyecto-7/vehicles_us.c
 #Encabezado de la app
 st.header('ANALISIS DE VEHICULOS')
 
+#Boton de histograma
 if st.button("Mostrar histograma"):
     fig = px.histogram(car_data, x="odometer")  # ajusta la columna
     st.plotly_chart(fig)
+
+#boton de grafico de dispersion
+if st.button('Generar Gráfico de Dispersión'):
+    fig_scatter = px.scatter(car_data, x="odometer", y="price", title="Gráfico de Dispersión de Odómetro vs Precio")
+    st.write(fig_scatter)
+    st.plotly_chart(fig_scatter)
 
